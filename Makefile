cec6d19a4ba5426c6d5c7b02ea1af225ac2a2ffc
Thomas Muntaner <thomas.muntaner@gmail.com>
2020-01-27 16:24:24 +0100

    Test next page functionality

--- Makefile
+++ Makefile
@@ -1,11 +1,10 @@
-NAME    = registry-tools
-VERSION = 0.0.1
-
 default: build
 
+.PHONY: clean
 clean:
 	rm -f _output/*
 
+.PHONY: build
 build: clean
 	go build -o _output -mod vendor ./cmd/...
 	tar -cvzf _output/vendor.tar.gz vendor
--- internal/registry/repository_list_test.go
+++ internal/registry/repository_list_test.go
@@ -23,6 +23,45 @@ func TestRepositoryList(t *testing.T) {
 	assert.Equal(t, expected, repositories)
 }
 
+func TestRepositoryListMultipage(t *testing.T) {
+	expected := []string{"foo", "bar"}
+	mux := http.NewServeMux()
+	mux.HandleFunc("/2", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := json.Marshal(repoListResponse{Repositories: []string{"bar"}})
+		w.WriteHeader(200)
+		_, _ = w.Write(body)
+	})
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		body, _ := json.Marshal(repoListResponse{Repositories: []string{"foo"}})
+		w.Header().Add("Link", "</2>;")
+		w.WriteHeader(200)
+		_, _ = w.Write(body)
+	})
+
+	ts := httptest.NewServer(mux)
+	defer ts.Close()
+
+	repositories, err := RepositoryList(ts.URL)
+	assert.Nil(t, err)
+	assert.Equal(t, expected, repositories)
+}
+
+func TestRepositoryListMultipageWithError(t *testing.T) {
+	expected := []string{"foo", "bar"}
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		body, _ := json.Marshal(repoListResponse{Repositories: expected})
+		w.Header().Add("Link", "<foo")
+		w.WriteHeader(200)
+		_, _ = w.Write(body)
+	}))
+
+	defer ts.Close()
+
+	repositories, err := RepositoryList(ts.URL)
+	assert.NotNil(t, err)
+	assert.Equal(t, expected, repositories)
+}
+
 func TestRepositoryListWithError(t *testing.T) {
 	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 	}))
